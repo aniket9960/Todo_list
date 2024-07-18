28dbb4fb6126a9b785bc195d38dba5283b1a9ac1
Aniket Jagtap <aniketjagtap9960@gmail.com>
2024-07-18 21:43:10 +0530

    Task form added

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-todo-list',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.css'
 })
 export class TodoListComponent {
+
+  constructor(private formBuilder : FormBuilder){}
+
+  taskFrom = this.formBuilder.group({
+    task : ['',[Validators.required]]
+  });
+
+  onSubmit(){
+    alert(this.taskFrom.value.task);
+  };
+
 
 }
